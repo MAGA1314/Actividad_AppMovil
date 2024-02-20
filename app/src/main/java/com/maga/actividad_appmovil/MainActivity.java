3f827e429d8371c849eda26955a5ecc5c070dab4
MAGA1314 <garciaagudelomiguelangel@gmail.com>
2024-02-20 18:19:33 -0500

    Funciones de menajes en diferentes estados de uso

--- app/src/main/java/com/maga/actividad_appmovil/MainActivity.java
+++ app/src/main/java/com/maga/actividad_appmovil/MainActivity.java
@@ -3,6 +3,7 @@ package com.maga.actividad_appmovil;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    String Tag = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,5 +41,40 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(Tag,"Estoy en OnStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(Tag,"Bienvenido de nuevo");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(Tag,"Estoy en OnPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(Tag,"Estoy en OnStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(Tag,"Estoy en OnRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(Tag,"Estoy en OnDestroy");
     }
 }
